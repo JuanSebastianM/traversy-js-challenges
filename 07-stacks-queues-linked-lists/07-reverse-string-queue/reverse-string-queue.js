ffe4913d2e7341ed1b7f0070e1db6b312aaedc60
Juan Mendoza <cebo_052000@hotmail.com>
2024-01-08 12:07:13 -0500

    add queue implementation and complete reverse string challenge with queue

--- 07-stacks-queues-linked-lists/07-reverse-string-queue/reverse-string-queue.js
+++ 07-stacks-queues-linked-lists/07-reverse-string-queue/reverse-string-queue.js
@@ -1,5 +1,21 @@
 const Queue = require('./queue');
 
-const reverseStringWithQueue = (str) => {};
+const reverseStringWithQueue = (str) => {
+  const queue = new Queue();
+  
+  for (let index = str.length - 1; index >= 0; index--) {
+    const lastChar = str[index];
+
+    queue.enqueue(lastChar);
+  }
+
+  let reversedString = "";
+
+  while (!queue.isEmpty()) {
+    reversedString += queue.dequeue();
+  }
+
+  return reversedString;
+};
 
 module.exports = reverseStringWithQueue;
