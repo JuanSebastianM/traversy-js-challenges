0ee9476eb0cd56a6652c9ba5d4741d787fb3f78a
Juan Mendoza <cebo_052000@hotmail.com>
2024-01-08 10:40:12 -0500

    create custom stack class

--- 07-stacks-queues-linked-lists/02-stack-implementation/stack.js
+++ 07-stacks-queues-linked-lists/02-stack-implementation/stack.js
@@ -1,3 +1,39 @@
-class Stack {}
+class Stack {
+  constructor() {
+    this.maxSize = 100;
+    this.stack = [];
+    this.top = -1;
+  }
+
+  push(item) {
+    if (this.isFull()) throw new Error('Max size exceeded.')
+
+    this.top++;
+    this.stack[this.top] = item;
+  }
+
+  pop() {
+    if (this.isEmpty()) return undefined;
+
+    this.top--;
+
+    return this.stack.pop();
+
+  }
+
+  peek() {
+    if (this.isEmpty()) return null;
+    
+    return this.stack[this.top]
+  }
+
+  isFull() {
+    return this.top === this.maxSize - 1;
+  }
+
+  isEmpty() {
+    return this.top === -1
+  }
+}
 
 module.exports = Stack;
