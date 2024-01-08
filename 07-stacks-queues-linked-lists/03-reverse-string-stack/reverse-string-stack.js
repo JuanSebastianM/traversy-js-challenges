b31bf28d104865a3576fe459bd320b9b83581eae
Juan Mendoza <cebo_052000@hotmail.com>
2024-01-08 11:02:54 -0500

    complete reverse string with stack class challenge

--- 07-stacks-queues-linked-lists/03-reverse-string-stack/reverse-string-stack.js
+++ 07-stacks-queues-linked-lists/03-reverse-string-stack/reverse-string-stack.js
@@ -1,5 +1,21 @@
 const Stack = require('./stack');
 
-function reverseStringStack() {}
+function reverseStringStack(string) {
+  const stringStack = new Stack();
+
+  const reversedCharacters = [];
+
+  for (const character of string) {
+    stringStack.push(character)
+  }
+  
+  for (let index = 0; index <= string.length; index++) {
+    const poppedChar = stringStack.pop();
+    
+    reversedCharacters.push(poppedChar)
+  }
+
+  return reversedCharacters.join("");
+}
 
 module.exports = reverseStringStack;
