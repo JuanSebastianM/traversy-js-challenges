7b2d76a5d4f630d4c61c3fc0b8031ef4fd143335
Juan Mendoza <cebo_052000@hotmail.com>
2024-01-08 14:36:12 -0500

    complete palindrome queue stack challenge

--- 07-stacks-queues-linked-lists/08-palindrome-queue-stack.js/palindrome-queue-stack.js
+++ 07-stacks-queues-linked-lists/08-palindrome-queue-stack.js/palindrome-queue-stack.js
@@ -1,6 +1,28 @@
 const Queue = require('./queue');
 const Stack = require('./stack');
 
-function isPalindromeQueueStack() {}
+function isPalindromeQueueStack(string) {
+  const queue = new Queue();
+  const stack = new Stack();
+
+  const formattedString = string.toLowerCase().replace(/\W+/g, "");
+
+  for (let index = 0; index < formattedString.length; index++) {
+    const character = formattedString[index];
+    
+    queue.enqueue(character)
+    stack.push(character)
+  }
+
+  let originalStringFormat = "";
+  let reversedStringFormat = "";
+
+  while (!queue.isEmpty() && !stack.isEmpty()) {
+    originalStringFormat += queue.dequeue();
+    reversedStringFormat += stack.pop()
+  }
+
+  return originalStringFormat === reversedStringFormat;
+}
 
 module.exports = isPalindromeQueueStack;
