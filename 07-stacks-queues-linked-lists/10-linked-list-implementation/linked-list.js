7f80f3f8f2feda9bf7eb020911799c368730470a
Juan Mendoza <cebo_052000@hotmail.com>
2024-01-08 18:21:37 -0500

    add linked list implementation and complete reverse string challenge

--- 07-stacks-queues-linked-lists/10-linked-list-implementation/linked-list.js
+++ 07-stacks-queues-linked-lists/10-linked-list-implementation/linked-list.js
@@ -1,5 +1,112 @@
-class Node {}
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
 
-class LinkedList {}
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  add(data) {
+    const node = new Node(data);
+
+    if (this.head === null) {
+      this.head = node;
+    } else {
+      this.tail.next = node;
+    }
+
+    this.tail = node;
+  }
+
+  get(index) {
+    let current = this.head;
+
+    let currentNodeIndex = 0;
+
+    while (currentNodeIndex < index) {
+      current = current.next;
+      currentNodeIndex++;
+    }
+
+    return current.data;
+  }
+  
+  getByValue(value, head = this.head) {
+    if (head === null) return null;
+
+    if (head.data === value) return head;
+
+    return this.getByValue(value, head.next);
+  }
+
+  insertAt(index, data) {
+    if (index === 0) {
+      const node = new Node(data);
+      node.next = this.head;
+      this.head = node;
+    } else {
+      let current = this.head;
+      let previous = null;
+      let i = 0;
+
+      while (i < index) {
+        previous = current;
+        current = current.next;
+        i++
+      }
+
+      const node = new Node(data);
+      node.next = current;
+      previous.next = node;
+      
+    }
+  }
+
+  removeFrom(index) {
+    if (index === 0) {
+      this.head = this.head.next;
+    } else {
+      let current = this.head;
+      let previous = null;
+      let i = 0;
+
+      while (i < index) {
+        previous = current;
+        current = current.next;
+        i++
+      }
+
+      previous.next = current.next;
+
+    }
+  }
+
+  printAll() {
+    let current = this.head;
+
+    while (current !== null) {
+      console.log(current.data);
+      current = current.next;
+    }
+  }
+
+  countNodes() {
+    let current = this.head;
+
+    let count = 0;
+
+    while (current !== null) {
+      current = current.next;
+      count++
+    }
+
+    return count;
+  }
+}
 
 module.exports = { Node, LinkedList };
--- 07-stacks-queues-linked-lists/11-reverse-string-linked-list/reverse-string-linked-list.js
+++ 07-stacks-queues-linked-lists/11-reverse-string-linked-list/reverse-string-linked-list.js
@@ -1,5 +1,22 @@
 const { LinkedList } = require('./linked-list');
 
-function reverseStringLinkedList() {}
+function reverseStringLinkedList(str) {
+  const linkedList = new LinkedList();
+
+  for (const char of str) {
+    linkedList.add(char);
+  }
+
+  let reversedString = "";
+  let index = str.length - 1;
+
+  while (index >= 0) {
+    reversedString += linkedList.get(index)
+
+    index--;
+  }
+
+  return reversedString;
+}
 
 module.exports = reverseStringLinkedList;
