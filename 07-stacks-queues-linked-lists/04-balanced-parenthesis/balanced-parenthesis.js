e3a2b80d5ffea1925ea656ffedf66ff1b326cdd6
Juan Mendoza <cebo_052000@hotmail.com>
2024-01-08 11:35:27 -0500

    complete balanced parentheses challenge

--- 07-stacks-queues-linked-lists/04-balanced-parenthesis/balanced-parenthesis.js
+++ 07-stacks-queues-linked-lists/04-balanced-parenthesis/balanced-parenthesis.js
@@ -1,5 +1,19 @@
 const Stack = require('./stack');
 
-function balancedParenthesis() {}
+function balancedParenthesis(string) {
+  const stack = new Stack();
+
+  for (const parenthesis of string) {
+    if (parenthesis === "(") {
+      stack.push(parenthesis)
+    } else {
+      if (stack.isEmpty()) return false;
+
+      stack.pop()
+    }
+  }
+
+  return stack.isEmpty()
+}
 
 module.exports = balancedParenthesis;
