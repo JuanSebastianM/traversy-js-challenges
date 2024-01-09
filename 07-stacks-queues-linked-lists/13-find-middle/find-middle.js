3c79ca4d2f19e9f6a8bb50c9f15a978a7c8eaed0
Juan Mendoza <cebo_052000@hotmail.com>
2024-01-09 16:01:16 -0500

    complete find middle challenge with fast and slow pattern

--- 07-stacks-queues-linked-lists/13-find-middle/find-middle.js
+++ 07-stacks-queues-linked-lists/13-find-middle/find-middle.js
@@ -1,3 +1,13 @@
-function findMiddle() {}
+function findMiddle(linkedList) {
+  let fast = linkedList.head;
+  let slow = linkedList.head;
+
+  while (fast !== null && fast.next !== null) {
+    fast = fast.next.next;
+    slow = slow.next;
+  }
+
+  return slow;
+}
 
 module.exports = findMiddle;
