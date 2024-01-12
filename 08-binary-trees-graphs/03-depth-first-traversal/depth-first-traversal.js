1cdfe7d75bdc75577ac8542be95d3480e93edbce
Juan Mendoza <cebo_052000@hotmail.com>
2024-01-12 11:33:13 -0500

    add depth first traversal code

--- 08-binary-trees-graphs/03-depth-first-traversal/depth-first-traversal.js
+++ 08-binary-trees-graphs/03-depth-first-traversal/depth-first-traversal.js
@@ -1,8 +1,64 @@
 const Stack = require('./stack');
 
-class Node {}
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
 
-function depthFirstTraversal() {}
+// with normal array as the stack
+// function depthFirstTraversal(root) {
+//   if (!root) return [];
+
+//   const result = [];
+//   const stack = [];
+
+//   stack.push(root);
+
+//   while (stack.length > 0) {
+//     const current = stack.pop();
+
+//     result.push(current.value)
+
+//     if (current.right) {
+//       stack.push(current.right)
+//     }
+
+//     if (current.left) {
+//       stack.push(current.left)
+//     }
+//   }
+
+//   return result;
+// }
+
+// with class Stack as the stack
+function depthFirstTraversal(root) {
+  if (!root) return [];
+
+  const result = [];
+  const stack = new Stack();
+
+  stack.push(root);
+
+  while (!stack.isEmpty()) {
+    const current = stack.pop();
+
+    result.push(current.value)
+
+    if (current.right) {
+      stack.push(current.right)
+    }
+
+    if (current.left) {
+      stack.push(current.left)
+    }
+  }
+
+  return result;
+}
 
 module.exports = {
   Node,
