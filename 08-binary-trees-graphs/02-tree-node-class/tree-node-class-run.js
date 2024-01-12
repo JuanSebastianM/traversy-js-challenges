c09a2d3ed89676c64728aaf288ec3a14d284eb09
Juan Mendoza <cebo_052000@hotmail.com>
2024-01-12 11:07:45 -0500

    add tree node class to create binary trees manually

--- 08-binary-trees-graphs/02-tree-node-class/tree-node-class-run.js
+++ 08-binary-trees-graphs/02-tree-node-class/tree-node-class-run.js
@@ -1,1 +1,21 @@
 const TreeNode = require('./tree-node-class');
+
+const a = new TreeNode('a')
+const b = new TreeNode('b');
+const c = new TreeNode('c');
+const d = new TreeNode('d');
+const e = new TreeNode('e');
+const f = new TreeNode('f');
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+
+console.log({a});
+console.log({b});
+console.log({c});
+console.log({d});
+console.log({e});
+console.log({f});
