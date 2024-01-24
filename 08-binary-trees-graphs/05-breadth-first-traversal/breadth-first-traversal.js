76d7dbe09affde87c447cecd147d785873fcc474
Juan Mendoza <cebo_052000@hotmail.com>
2024-01-24 16:26:58 -0500

    complete breadth first traversal challenge

--- 08-binary-trees-graphs/05-breadth-first-traversal/breadth-first-traversal.js
+++ 08-binary-trees-graphs/05-breadth-first-traversal/breadth-first-traversal.js
@@ -1,8 +1,69 @@
 const Queue = require('./queue');
 
-class Node {}
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
 
-function breadthFirstTraversal() {}
+// function breadthFirstTraversal(root) {
+//   if (!root) {
+//     return [];
+//   };
+  
+//   const result = [];
+//   const queue = [];
+
+//   queue.push(root);
+
+//   while (queue.length > 0) {
+//     const head = queue.shift();
+
+//     result.push(head.data);
+
+//     if (head.left) {
+//       queue.push(head.left)
+//     }
+
+//     if (head.right) {
+//       queue.push(head.right)
+//     }
+
+//   }
+  
+//   return result;
+// }
+
+// using the Queue class
+function breadthFirstTraversal(root) {
+  if (!root) {
+    return [];
+  };
+  
+  const result = [];
+  const queue = new Queue();
+
+  queue.enqueue(root);
+
+  while (!queue.isEmpty()) {
+    const head = queue.dequeue();
+
+    result.push(head.data);
+
+    if (head.left) {
+      queue.enqueue(head.left)
+    }
+
+    if (head.right) {
+      queue.enqueue(head.right)
+    }
+
+  }
+  
+  return result;
+}
 
 module.exports = {
   Node,
